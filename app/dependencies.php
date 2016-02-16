5d76c1f26e2ccaca2b504cf7bcddd563320baa7b
Eric Pierce <epierce@usf.edu>
2016-02-16 11:35:09 -0500

    Add example service

--- app/dependencies.php
+++ app/dependencies.php
@@ -3,43 +3,78 @@
  *
  * Configure Dependency Injection
  * see: http://www.slimframework.com/docs/concepts/di.html
- */
+ *
+ **/
 
 $container = $app->getContainer();
 $settings = $container->settings;
 
-// -----------------------------------------------------------------------------
-// Service providers
-// -----------------------------------------------------------------------------
-
-// Twig
+// Twig templating engine: http://twig.sensiolabs.org/documentation
 $container['view'] = function ($c) use ($settings) {
-    $view = new \Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
 
-    // Add extensions
+    $default_config = [
+      'cache'            => '../cache',
+      'debug'            => false,
+      'auto_reload'      => true,
+      'autoescape'       => true,
+      'strict_variables' => false
+    ];
+
+    $path = $settings['view']['template_path'] ?? '../templates';
+    $twig_settings = $settings['view']['twig'] ?? $default_config;
+
+    // Create Twig object and add Slim extensions
+    $view = new \Slim\Views\Twig($path, $twig_settings);
     $view->addExtension(new Slim\Views\TwigExtension($c->router, $c->request->getUri()));
     $view->addExtension(new Twig_Extension_Debug());
 
     return $view;
 };
 
-// Monolog
+// Monolog logging service: https://github.com/Seldaek/monolog
 $container['logger'] = function ($c) use ($settings) {
-    $logger = new \Monolog\Logger($settings['logger']['name']);
+    $name = $settings['logger']['name'] ?? __FILE__;
+    $location = $settings['logger']['path'] ?? '../logs/application.log';
+    $log_level = $settings['logger']['level'] ?? 'DEBUG';
+
+    $logger = new \Monolog\Logger($name);
     $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['logger']['path'], \Monolog\Logger::DEBUG));
+    $logger->pushHandler(new \Monolog\Handler\StreamHandler($location, \Monolog\Logger::toMonologLevel($log_level)));
+
     return $logger;
 };
 
-// Example Service
+/**
+ * -----------------------------------------------------------------------------
+ * Service Layer
+ * -----------------------------------------------------------------------------
+ *
+ * These classes provide the business logic to interact with services and build the information necessary for
+ * views.  View routes.php to see how these classes are mapped to URLs
+ */
+
 $container['myService'] =  function ($c) {
-    return new epierce\slimSkeleton\Service\exampleService($c->logger, $c->settings);
+    return new \USF\IdM\SlimSkeleton\Service\ExampleService($c->logger, $c->settings);
 };
 
-// -----------------------------------------------------------------------------
-// Action factories
-// -----------------------------------------------------------------------------
+/**
+ * -----------------------------------------------------------------------------
+ * Action factories (Controllers)
+ * -----------------------------------------------------------------------------
+ *
+ * These classes provide the business logic to interact with services and build the information necessary for
+ * views.  All of them get the view, logging, and settings components along with any other neccesary services passed in
+ * as arguments to the class constructors.
+ *
+ * View routes.php to see how these classes are mapped to URLs
+ */
 
-$container['PeopleSoftAuthenticator\Action\HomeAction'] = function ($c) {
-    return new \USF\IdM\PeopleSoftAuthenticator\Action\HomeAction($c->view, $c->logger, $c->settings, $c->myService);
+// Application Home Page
+$container['SlimSkeleton\Action\HomeAction'] = function ($c) {
+    return new \USF\IdM\SlimSkeleton\Action\HomeAction($c->view, $c->logger, $c->settings);
+};
+
+//// Example Controller with a Service
+$container['SlimSkeleton\Action\ExampleAction'] = function ($c) {
+    return new \USF\IdM\SlimSkeleton\Action\ExampleAction($c->view, $c->logger, $c->settings, $c->myService);
 };
--- app/src/Service/ExampleService.php
+++ app/src/Service/ExampleService.php
@@ -2,33 +2,30 @@
 /**
  * Example Service
  *
- * @category epierce
- * @package slim-skeleton
+ * @category USF-IT
+ * @package slimSkeleton
  * @author Eric Pierce <epierce@usf.edu>
  * @license http://www.opensource.org/licenses/MIT MIT
- * @link https://github.com/epierce/slim-skeleton
+ * @link https://github.com/USF-IT/slim-skeleton
  */
-namespace USF\IdM\PeopleSoftAuthenticator\Service;
+namespace USF\IdM\SlimSkeleton\Service;
 
-use USF\IdM\UsfEncryption;
-use USF\IdM\CLients\NamsIdentifierConversionClient;
 use Psr\Log\LoggerInterface;
 use Slim\Collection;
-use League\Uri\Schemes\Http as HttpUri;
+use GuzzleHttp\Client;
 
 /**
- * Identify users and generate authentication tokens for PeopleSoftAuthenticator
  *
- * @category USF/IT
- * @package PeopleSoftAuthenticator
+ *
+ * @category USF-IT
+ * @package slimSkeleton
  * @author Eric Pierce <epierce@usf.edu>
- * @license http://www.apache.org/licenses/LICENSE-2.0 Apache2.0
- * @link https://github.com/USF-IT/PeopleSoftAuthenticator
+ * @license http://www.opensource.org/licenses/MIT MIT
+ * @link https://github.com/USF-IT/slim-skeleton
  */
-class PeopleSoftService
+class ExampleService
 {
     private $logger;
-    private $client;
     private $settings;
 
     public function __construct(LoggerInterface $logger, Collection $settings )
@@ -36,115 +33,24 @@ class PeopleSoftService
         $this->logger = $logger;
         $this->settings = $settings;
 
-        $nams_config = $settings['nams'];
-        $this->client = new NamsIdentifierConversionClient($nams_config['restCasClient']);
-        $this->client->setNamsHost($nams_config['host']);
-    }
+        // Pull config data from the settings object
+        $serviceConfig = $settings['example_config'];
 
-
-    /**
-     * Get list of usernames for this user from NAMS
-     *
-     * @param string $application PeopleSoft Application
-     * @param string $netid       USF NetID
-     * @return array
-     * @throws \Exception
-     */
-    public function getUsernameList($application, $netid)
-    {
-        $results = $this->client->convertIdentifier('netid', 'accounts', $netid);
-
-        $this->logger->debug("ws_convert result: ". json_encode($results));
-
-        $hostType = $this->settings['accountTypes'][$application];
-
-        $accountList = [];
-
-        if (isset($results[$hostType])) {
-            foreach ($results[$hostType] as $entry) {
-                $accountList[] = $entry['username'];
-            }
-        }
-
-        return $accountList;
-
+        // If you need to configure your service object, do it here and  add it as a
+        // private property to the class.
     }
 
     /**
-     * Verify that a given username is in the list of accounts owned by this user
+     * Get the MD5 hash of the string from a public webservice
      *
-     * @param string $application      PeopleSoft Application
-     * @param string $netid            USF NetID
-     * @param string $selectedUsername Check for this username
-     * @return bool
+     * @param $input_text
+     * @return mixed
      */
-    public function verifyAccountOwnership($application, $netid, $selectedUsername)
+    public function getMD5fromWS($input_string)
     {
-        $psUsernameList = $this->getUsernameList($application, $netid);
+        $client = new Client();
+        $data = json_decode($client->get('http://md5.jsontest.com/?text='.$input_string)->getBody(), true);
 
-        return in_array($selectedUsername, $psUsernameList);
-    }
-
-    /**
-     * Generate an authentication URL for a PeopleSoft Application
-     *
-     * @param string $application      PeopleSoft Application
-     * @param string $instance         PeopleSoft instance
-     * @param string $netid            USF NetID
-     * @param string $ipAddress        Client IP
-     * @param string $deepLink         Page within People application to send the user to
-     * @param string $selectedUsername Use this username for the service
-     * @return string
-     * @throws MultipleAccountsFoundException
-     * @throws NoAccountFoundException
-     * @throws \Exception
-     */
-    public function getRedirectUrl($application, $instance, $netid, $ipAddress, $deepLink = '', $selectedUsername = '')
-    {
-        $key = $this->settings['instances'][$instance][$application]['encryptionKey'];
-        $blockType = $this->settings['instances'][$instance][$application]['encryptionBlockType'];
-
-        if ($selectedUsername != '' && $this->verifyAccountOwnership($application, $netid, $selectedUsername)) {
-            $psUsernameList = [$selectedUsername];
-        } else {
-            $psUsernameList = $this->getUsernameList($application, $netid);
-        }
-
-        if (count($psUsernameList) == 0) {
-            throw new NoAccountFoundException("No accounts were found in ${application} (${instance}) for ${netid}");
-        } elseif (count($psUsernameList) > 1) {
-            $allowMulti = $this->settings['instances'][$instance][$application]['allowMultipleAccounts'] ?? false;
-            if ($allowMulti) {
-                return $psUsernameList;
-            }
-            throw new MultipleAccountsFoundException("Multiple accounts were found in ${application} (${instance}) for ${netid}: " . json_encode($psUsernameList));
-        } else {
-            $psUsername = $psUsernameList[0];
-        }
-
-        $plainToken = time()."|${application}|${ipAddress}|${psUsername}";
-        $encryptedToken = urlencode(UsfEncryption::encrypt($key, $plainToken, $blockType));
-        $this->logger->debug("Token plaintext: ${plainToken} | Final: ${encryptedToken}");
-
-        $psPath = $this->settings['instances'][$instance][$application]['appPath'];
-        $psQuery = ['cmd' => 'start', 'user' => $psUsername, 'app' => $application, 'token' => $encryptedToken];
-        if ($deepLink !== '') {
-            unset($psQuery['cmd']);
-            $psQuery['NAVSTACK'] = 'Clear';
-            $psPath = $psPath . $this->settings['instances'][$instance][$application]['deepLinkPath'] . $deepLink;
-        }
-
-        $url = HttpUri::createFromString('');
-        $url = $url->withHost($this->settings['instances'][$instance][$application]['host'])
-                   ->withScheme('https')
-                   ->withPort($this->settings['instances'][$instance][$application]['port'])
-                   ->withPath($psPath)
-                   ->withQuery((string) \League\Uri\Components\Query::createFromArray($psQuery));
-
-        return (string) $url;
+        return $data['md5'];
     }
 }
-
-class NoAccountFoundException extends \Exception { }
-
-class MultipleAccountsFoundException extends \Exception { }
